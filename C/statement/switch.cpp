f2136b508e90ea8ec5cfcd6a68d49637721f1d1e
gyb357 <gyb357@naver.com>
2022-10-11 21:25:55 +0900

    no message

---
+++ C/statement/switch.cpp
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+int main() {
+	/*
+	//--------------------------------//
+	
+	int a;
+	
+	printf("a값울 입력하시오: ");
+	scanf("%d",&a);
+
+	if (a > 90) {
+		printf("A\n");
+	} else if (a > 80) {
+		printf("B\n");
+	} else {
+		printf("C\n");
+	}
+	
+	//--------------------------------//
+	
+	int score = 8, bonus = 5;
+
+	if (score >= 10) {
+		bonus = bonus + 1;
+	}
+	score = score + bonus;
+
+	printf("score = %d, bonus = %d\n", score, bonus);
+	
+	//--------------------------------//
+	
+	int  score;
+
+	printf("점수를 입력하시오.: ");
+	scanf("%d", &score);
+
+	if (score > 60) {
+		printf("degree = D \n");
+	} else if (score > 70) {
+		printf("degree = C");
+	} else if (score > 80) {
+		printf("degree = C");
+	} else if (score > 90) {
+		printf("degree = A");
+	} else {
+		printf("degree = F");
+	}
+	
+	//--------------------------------//
+	
+	char i;
+
+	printf("문자를 입력하시오.: ");
+	scanf("%c", &i);
+
+	switch (i) {
+		case 'a': printf("'a'\n"); break;
+		case 'b': printf("'b'\n"); break;
+		case 'c': printf("'c'\n"); break;
+		case 'd': printf("'d'\n"); break;
+		default: printf("Error\n"); break;
+	}
+	
+	//--------------------------------//
+	
+	int i;
+
+	printf("enter the number.: ");
+	scanf("%d", &i);
+
+	if (i < 0) {
+		printf("number is negative.\n");
+	}
+	if (i >= 0) {
+		printf("number is positive.\n");
+	}
+	
+	//--------------------------------//
+	
+	int x, y;
+	char xy;
+
+	printf("**** result ****\n\n");
+	printf("Input x xy == ?");
+	scanf("%d %c %d", &x, &xy, &y);
+
+	if (xy == '*') { printf("%d * %d = %d\n", x, y, x * y); }
+	if (xy == '/') {printf("%d / %d = %d\n", x, y, x / y);}
+	if (xy == '+') {printf("%d + %d = %d\n", x, y, x + y);}
+	if (xy == '-') {printf("%d - %d = %d\n", x, y, x - y);}
+
+	
+	//--------------------------------//
+	
+	*/
+	return 0;
+}
