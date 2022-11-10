fd26b07fccc2a5393b6b3ac3274788543b720424
gyb357 <gyb357@naver.com>
2022-11-10 12:36:34 +0900

    no message

---
+++ R/homework/Week11/1110_02.R
@@ -0,0 +1,49 @@
+# 히스토그램
+data()
+cars
+str(cars)
+
+
+
+# cars 1열
+cars[, 1]
+cars[, "speed"]
+cars$speed
+sp <- cars$speed
+
+
+
+# cars 2열
+cars[, 2]
+cars[, "dist"]
+cars$dist
+ds <- cars$dist
+
+
+
+# 히스토그램 만들기
+# 변수로 두면 상태값을 계산함
+st <- hist(ds, main = '',
+               col = c('#003f5c', '#58508d', '#bc5090', '#ff6361', '#ffa600'),
+               breaks = 5,            # 막대 개수
+               xlab = '제동거리',     # x축 이름
+               ylab = '빈도수',       # y축 이름
+               border = 'palegreen1', # 막대 테두리 색
+               las = 2)               # x축 글씨 방향(0 ~ 3)
+st
+# breaks:  각 구간 x축의 값
+# count:   각 막대의 y값
+# density: 밀도
+# mid:     각 구간 x축의 중간값
+# xname:   히스토그램 데이터 변수 이름
+
+
+
+# count
+ff <- st$counts
+ff
+# 1을 빼서 x축 이름이 20부터 시작하도록 함
+names(ff) <- st$breaks[-1]
+ff
+
+
---
+++ R/homework/Week11/1110_04.R
@@ -0,0 +1,35 @@
+# 과제
+# 정책 지지도 그래프 그리기
+# 1. carData 패키지 안에 있는 Chile데이터셋을 불러와 변수 ds에 저장합니다.
+install.packages('carData')
+library(carData)
+
+
+
+# 데이터 확인
+ds <- Chile
+ds
+str(ds)
+
+
+
+# 2. 화면을 2행 3열의 레이아웃으로 분할합니다.
+par(mfcol = c(2, 3))
+color <- rainbow(20)
+color
+
+
+
+# 3. 첫 번째 행에 지역별,성별,교육수준별 분포에 대한 3개의 막대그래프를 작성합니다.
+barplot(table(ds$region),    main = '지역별 분포',      col = color[1:5])
+barplot(table(ds$sex),       main = '성별 분포',        col = rainbow(5))
+barplot(table(ds$education), main = '교육수준별 분포',  col = color[5:10])
+
+
+
+# 4. 두 번째 행에 연령,수입,정책 지지도에 대한 3개의 히스토그램을 작성합니다
+hist(ds$age,       breaks = 6, main = '연령',        xlab = 'age',     col = color[5:10])
+hist(ds$income,    breaks = 4, main = '수입',        xlab = 'income',  col = color[10:15])
+hist(ds$statusquo, breaks = 9, main = '정책 지지도', xlab = 'support', col = color[15:20])
+
+
